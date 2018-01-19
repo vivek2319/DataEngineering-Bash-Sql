75333fd4ca097360f419d39c0616c39c079ccf0e
Vivek Kulkarni <vivek2319@gmail.com>
2018-01-19 12:34:18 -0500

    Create Intereactive-name.sh

---
+++ bin/Intereactive-name.sh
@@ -0,0 +1,6 @@
+#This is interactive shell script, and will read name from user and display it
+#Reading your name 
+#!/bin/bash
+echo -n "Enter your name: "
+read name
+echo You entered Mr./Ms. $name
