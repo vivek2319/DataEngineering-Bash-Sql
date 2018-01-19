142f83d311473fb23804ef712a56997aef23a33e
Vivek Kulkarni <vivek2319@gmail.com>
2018-01-19 12:17:41 -0500

    Create task.sh

---
+++ bin/task.sh
@@ -0,0 +1,18 @@
+#This script is written to do basic tasks
+#!/bin/bash
+
+#Display the domain name
+hostname -d
+#Display IP Address
+hostname -i
+#Display current week number 
+date +%u
+#Display Linux Flav Name
+uname -s
+#Display Name of the processor
+uname -p
+#Display Date in yyyy/mm/dd
+date +%F
+#Display DayNumber in a year
+cal -j
+echo All commands successfully compiled   
