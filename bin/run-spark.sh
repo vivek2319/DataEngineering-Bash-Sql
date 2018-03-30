3940a724d7dfc3763b96ddebab45c81b92c6a53c
Vivek Kulkarni <vivek2319@gmail.com>
2018-03-30 12:22:20 -0400

    Create run-spark.sh

---
+++ bin/run-spark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#READ and USE WISELY -> Last is file name
+#gcloud dataproc jobs submit pyspark --cluster=mycluster --bucket=dataengine --region=us-central1 process.py 
+
+#gcloud dataproc jobs submit pyspark --cluster second --region us-central1 progamattachid.py
+
+#gcloud dataproc jobs submit pyspark --cluster second --region us-central1 revgame2.py
